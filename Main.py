f3ab69f95f2ade2e2467ed58c626f33996ea3789
Txai Wieser <txaidw@gmail.com>
2018-05-11 20:51:41 -0300

    Adding Main.py

---
+++ Main.py
@@ -0,0 +1,44 @@
+#
+# Versão do Python: 3.6.5
+#
+# Trabalho LINGUAGENS FORMAIS E AUTOMATOS
+#
+# Grupo:
+# 00230091 - Rodrigo Rech
+# 00229724 - Flávio Keglevich
+# 00217052 - Txai Wieser
+#
+
+import PersistenceHandler
+import GrammarReader
+
+def showIntro():
+    print(
+    	"""
+    	#########################################################################
+    	##                                                                     ##
+    	##                         Trabalho Prático                            ##
+    	##                   Linguagens Formais e Autômatos                    ##
+    	##                                                                     ##
+    	#########################################################################
+    	"""
+    	)
+
+def showMenu():
+    print("1 - Leitor da Gramática")
+    option = input("Escolha uma opção: ")
+    option = str(option)
+    out = "0"
+
+    if option == "1":
+        GrammarReader.start()
+        out = GrammarReader.showMenu()
+    else:
+        print("Exit")
+
+    if out == "-1":
+        showMenu()
+
+
+showIntro()
+showMenu()
